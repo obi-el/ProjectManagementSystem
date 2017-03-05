30b9391075a913e4bc12322498e65c92774fc3ca
Obinna <obi_el@yahoo.com>
2017-03-05 18:29:44 -0500

    Adding Spring dependencies

--- src/main/java/PMS/Application.java
+++ src/main/java/PMS/Application.java
@@ -1,7 +1,17 @@
 package PMS;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by zeningjiang on 3/2/2017.
  */
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
