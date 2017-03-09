e49b4a2167069d2df3d695c54b3892a441ae561c
jiangzening1993 <jiangzening1993@gmail.com>
2017-03-08 22:02:58 -0500

    Login page and home page setup

--- src/main/java/PMS/MvcConfig.java
+++ src/main/java/PMS/MvcConfig.java
@@ -3,6 +3,7 @@ package PMS;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * Created by Jiang on 08/03/2017.
  */
@@ -13,6 +14,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("login").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/test").setViewName("test");
     }
+
+
 }
--- src/main/java/PMS/WebSecurityConfig.java
+++ src/main/java/PMS/WebSecurityConfig.java
@@ -5,15 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.persistence.EntityManagerFactory;
 
 /**
  * Created by Jiang on 08/03/2017.
  */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebMvcConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -21,12 +24,13 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/page")
+                .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
     }
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
