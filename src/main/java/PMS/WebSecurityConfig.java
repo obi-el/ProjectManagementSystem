793c9f374642b371bfa38d11c5a1af78c5700d58
zeningjiang <jiangzening1993@gmail.com>
2017-03-19 16:46:23 -0400

    student page setup

--- src/main/java/PMS/WebSecurityConfig.java
+++ src/main/java/PMS/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/signin", "/error", "/studentPage","/register").permitAll()
+                .antMatchers("/", "/error", "/register", "/signin").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
