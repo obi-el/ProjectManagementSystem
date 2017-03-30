594b1b7fbbaf12c11d211cf49642a5b6eefa98e1
Obinna <obi_el@yahoo.com>
2017-03-30 10:37:15 -0400

    Backend notify unregistered students

--- src/main/java/PMS/Coordinator.java
+++ src/main/java/PMS/Coordinator.java
@@ -1,7 +1,16 @@
 package PMS;
 
+import com.sun.mail.smtp.SMTPSaslAuthenticator;
+
+import javax.mail.*;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.Entity;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 
 /**
  * Created by yashpatel on 3/8/2017.
@@ -11,30 +20,49 @@ public class Coordinator extends User{
 
     public Coordinator(){super();}
 
-    private ArrayList<Student> students;
 
     public Coordinator(String firstName, String lastName, String email, String password){
         super(firstName, lastName, email, password);
-        students = new ArrayList<Student>();
+
     }
 
-    public void createDeliverable(){
-    }
 
-    public ArrayList<Student> findUnassignedStudents(){
-        ArrayList<Student> UAS = new ArrayList<Student>();
-        for (Student s : students){
-            if (s.getMyProject() == null)
-                UAS.add(s);
+
+    public void contactUnassignedStudents(List<Student> Students) throws Exception{
+        String from = this.email;
+        List<String> to = new ArrayList<String>();
+        String host = "localhost";
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        for(Student s: Students){
+            if(s.getMyProject() == null){
+                to.add(s.getEmail());
+            }
         }
-        return UAS;
-    }
 
-    public void contactUnassignedStudents(){
-        ArrayList<Student> UAS = findUnassignedStudents();
-        for(Student s : UAS){
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
 
-            /** email a reminder to each student */
-        }
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            for(String s: to) message.addRecipient(Message.RecipientType.TO, new InternetAddress(s));
+
+            // Set Subject: header field
+            message.setSubject("Reminder!");
+
+            // Now set the actual message
+            message.setText("Do not forget to register in a project" +"\n \n" + this.getFirstName() + " "+ this.getLastName());
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+
+
+
     }
 }
